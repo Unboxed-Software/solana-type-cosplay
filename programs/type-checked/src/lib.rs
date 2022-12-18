39b4362f075f6c68507d4692f256d51a038ddcae
James Pacheco <jamesrp13@gmail.com>
2022-12-18 16:43:23 -0700

    Add solution

---
+++ programs/type-checked/src/lib.rs
@@ -0,0 +1,71 @@
+use anchor_lang::prelude::*;
+
+declare_id!("EU66XDppFCf2Bg7QQr59nyykj9ejWaoW93TSkk1ufXh3");
+
+#[program]
+pub mod type_checked {
+    use super::*;
+
+    pub fn initialize_admin(ctx: Context<InitializeAdmin>) -> Result<()> {
+        ctx.accounts.admin_config.admin = ctx.accounts.admin.key();
+        Ok(())
+    }
+
+    pub fn initialize_user(ctx: Context<InitializeUser>) -> Result<()> {
+        ctx.accounts.user_account.user = ctx.accounts.user.key();
+        Ok(())
+    }
+
+    pub fn update_admin(ctx: Context<UpdateAdmin>) -> Result<()> {
+        ctx.accounts.admin_config.admin = ctx.accounts.admin.key();
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct InitializeAdmin<'info> {
+    #[account(
+        init,
+        payer = admin,
+        space = 8 + 32
+    )]
+    pub admin_config: Account<'info, AdminConfig>,
+    #[account(mut)]
+    pub admin: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct InitializeUser<'info> {
+    #[account(
+        init,
+        payer = user,
+        space = 8 + 32
+    )]
+    pub user_account: Account<'info, User>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct UpdateAdmin<'info> {
+    #[account(
+        mut,
+        has_one = admin
+    )]
+    pub admin_config: Account<'info, AdminConfig>,
+    pub new_admin: SystemAccount<'info>,
+    #[account(mut)]
+    pub admin: Signer<'info>,
+}
+
+#[account]
+pub struct AdminConfig {
+    admin: Pubkey,
+}
+
+#[account]
+pub struct User {
+    user: Pubkey,
+}
--- tests/solana-type-cosplay.ts
+++ tests/solana-type-cosplay.ts
@@ -1,6 +1,7 @@
 import * as anchor from "@project-serum/anchor"
 import { Program } from "@project-serum/anchor"
 import { TypeCosplay } from "../target/types/type_cosplay"
+import { TypeChecked } from "../target/types/type_checked"
 import { expect } from "chai"
 
 describe("type-cosplay", () => {
@@ -8,9 +9,13 @@ describe("type-cosplay", () => {
   anchor.setProvider(provider)
 
   const program = anchor.workspace.TypeCosplay as Program<TypeCosplay>
+  const programChecked = anchor.workspace.TypeChecked as Program<TypeChecked>
 
   const userAccount = anchor.web3.Keypair.generate()
   const newAdmin = anchor.web3.Keypair.generate()
+
+  const userAccountChecked = anchor.web3.Keypair.generate()
+  const adminAccountChecked = anchor.web3.Keypair.generate()
 
   it("Initialize User Account", async () => {
     await program.methods
@@ -28,6 +33,54 @@ describe("type-cosplay", () => {
       .accounts({
         adminConfig: userAccount.publicKey,
         newAdmin: newAdmin.publicKey,
+      })
+      .rpc()
+  })
+
+  it("Initialize type checked AdminConfig Account", async () => {
+    await programChecked.methods
+      .initializeAdmin()
+      .accounts({
+        adminConfig: adminAccountChecked.publicKey,
+      })
+      .signers([adminAccountChecked])
+      .rpc()
+  })
+
+  it("Initialize type checked User Account", async () => {
+    await programChecked.methods
+      .initializeUser()
+      .accounts({
+        userAccount: userAccountChecked.publicKey,
+        user: provider.wallet.publicKey,
+      })
+      .signers([userAccountChecked])
+      .rpc()
+  })
+
+  it("Invoke update instruction using User Account", async () => {
+    try {
+      await programChecked.methods
+        .updateAdmin()
+        .accounts({
+          adminConfig: userAccountChecked.publicKey,
+          newAdmin: newAdmin.publicKey,
+          admin: provider.wallet.publicKey,
+        })
+        .rpc()
+    } catch (err) {
+      expect(err)
+      console.log(err)
+    }
+  })
+
+  it("Invoke update instruction using AdminConfig Account", async () => {
+    await programChecked.methods
+      .updateAdmin()
+      .accounts({
+        adminConfig: adminAccountChecked.publicKey,
+        newAdmin: newAdmin.publicKey,
+        admin: provider.wallet.publicKey,
       })
       .rpc()
   })
