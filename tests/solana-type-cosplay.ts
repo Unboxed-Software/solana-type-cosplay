684827d57d0cc3cb8b7777598b384728cbaf19de
0xCipherCoder <ciphercoder0x@gmail.com>
2024-08-30 21:17:40 +0530

    Updated code with anchor new version and initspace macro

--- tests/solana-type-cosplay.ts
+++ tests/solana-type-cosplay.ts
@@ -1,5 +1,5 @@
-import * as anchor from "@project-serum/anchor"
-import { Program } from "@project-serum/anchor"
+import * as anchor from "@coral-xyz/anchor";
+import { Program } from "@coral-xyz/anchor";
 import { TypeCosplay } from "../target/types/type_cosplay"
 import { expect } from "chai"
 
